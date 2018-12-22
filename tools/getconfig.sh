8bf0b8d6ff05de2518e0a956d0c0234531b1cfe5
Owen Barton <owen.barton@civicactions.com>
2018-12-21 17:07:24 -0800

    Need to actually build php-fpm.

--- tools/getconfig.sh
+++ tools/getconfig.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo export VERSION=\'$(php-config --version)\'
-echo export CONFIGURE_OPTIONS=\'$(php-config --configure-options)\'
+echo export CLI_CONFIGURE_OPTIONS=\'$(php-config --configure-options)\'
+echo export FPM_CONFIGURE_OPTIONS=\'$("$(dirname $(which php))/../sbin/php-fpm" -i | grep -F 'Configure Command' | cut -d"'" -f3- | sed -e "s/'//g")\'
 echo export PECL=\'$(pecl list | grep -F stable | awk '{print $1"-"$2}' | tr '\n' ' ')\'
 echo export EXTENSIONS=\'$(php -m | sed -e 's/\r//g' | grep -E '^[a-zA-Z0-9]+$' | tr '\n' ',')\'
 echo export ETC=\'$(php -i | grep -e 'Loaded Configuration File' | sed -e's/[^\/]*//' -e's/\/etc\/.*/\/etc/')\'
