f33d9d57cd9ab81d4b8afb374eca7a6a7a7e7127
Owen Barton <owen.barton@civicactions.com>
2018-12-26 15:21:06 -0800

    Adding some diagnostics.

--- drupal/test/test.sh
+++ drupal/test/test.sh
@@ -12,10 +12,11 @@ sleep 10
 
 # TODO: Actually install Drupal here and also test dumping and autoloading a database.
 
-echo "Fetching page to check status"
 HTTPD=$(docker-compose --file docker-compose-7.1.yml ps --quiet httpd)
 IP=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${HTTPD}")
+echo "Fetching page from ${IP} to check status"
 STATUS=$(curl --location --silent --output /dev/null --write-out "%{http_code}" "${IP}")
+echo "Status: ${STATUS}"
 if [ "${STATUS}" -ne "200" ]; then
   echo "httpd container not responding with 200 HTTP response code"
   exit 1
