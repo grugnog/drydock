d2a2ee9ccb965eaa0ea3e698607e65ce17449d22
Owen Barton <owen.barton@civicactions.com>
2018-12-26 15:35:37 -0800

    Use environment variables and do advance cleanup.

--- drupal/test/test.sh
+++ drupal/test/test.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
+export COMPOSE_FILE=docker-compose-7.1.yml
+export COMPOSE_PROJECT_NAME=drupal_acquia_${BRANCH_NAME}
+
+echo "Cleaning up any failed builds"
+docker-compose rm -f
+rm -rf docroot
+
 echo "Building containers"
-docker-compose --file docker-compose-7.1.yml build
+docker-compose build
 echo "Downloading Drupal core"
 docker run --volume "$(pwd)":/app --user $(id -u ${USER}):$(id -g ${USER}) drush/drush dl drupal -y --drupal-project-rename=docroot
 
 echo "Starting containers"
-docker-compose --file docker-compose-7.1.yml up -d
+docker-compose up --detach
 sleep 10
 
 # TODO: Actually install Drupal here and also test dumping and autoloading a database.
 
-HTTPD=$(docker-compose --file docker-compose-7.1.yml ps --quiet httpd)
+HTTPD=$(docker-compose --quiet httpd)
 IP=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${HTTPD}")
 echo "Fetching page from ${IP} to check status"
 STATUS=$(curl --location --silent --output /dev/null --write-out "%{http_code}" "${IP}")
@@ -23,5 +30,5 @@ if [ "${STATUS}" -ne "200" ]; then
 fi
 
 echo "Cleaning up"
-docker-compose --file docker-compose-7.1.yml rm -f
+docker-compose rm -f
 rm -rf docroot
