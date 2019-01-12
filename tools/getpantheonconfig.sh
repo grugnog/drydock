213bb297fff602c3689ef3b9ead927a884bad77e
Owen Barton <owen.barton@civicactions.com>
2019-01-11 16:22:34 -0800

    Build php 7.1 and 7.2 on Pantheon.

--- tools/getpantheonconfig.sh
+++ tools/getpantheonconfig.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
+set -euo pipefail
+SUPPORTED_PHP=( 7.1 7.2 )
 if [ -z "$SITE" ]; then
   echo 'Specify a Pantheon $SITE to get configuration for.'
   echo 'e.g. export SITE=mysitename'
-  echo 'Sites dev instance must be in sftp mode'
+  echo "Must have a multidev instance for each supported PHP version (${SUPPORTED_PHP[*]})"
+  echo '- PHP versions in each multidev must be configured with pantheon.yml.'
+  echo '- Multidevs should be named `PHPx-y` where x and y are the PHP major and minor version.'
+  echo '- Each multidev must be in sftp mode (not git mode).'
   exit 1
 fi
-UUID=$(terminus site:lookup "${SITE}")
-HOST=dev.${UUID}@appserver.dev.${UUID}.drush.in
-sftp -o Port=2222 "${HOST}":code/ <<< $'rm getconfig.php'
-(
-  cd "$( dirname "${BASH_SOURCE[0]}" )"
-  sftp -o Port=2222 "${HOST}":code/ <<< $'put getconfig.php'
-)
-terminus remote:drush "${SITE}.dev" php-script getconfig.php -- pantheon > currentconfig
-sftp -o Port=2222 "${HOST}":code/ <<< $'rm getconfig.php'
+for PHP in "${SUPPORTED_PHP[@]}"; do
+  MULTIDEV=php${PHP//[.]/-} 
+  echo "Getting config for $SITE -> $MULTIDEV"
+  UUID=$(terminus site:lookup "${SITE}")
+  HOST=${MULTIDEV}.${UUID}@appserver.${MULTIDEV}.${UUID}.drush.in
+  sftp -o Port=2222 "${HOST}":code/ <<< $'rm getconfig.php'
+  (
+    cd "$( dirname "${BASH_SOURCE[0]}" )"
+    sftp -o Port=2222 "${HOST}":code/ <<< $'put getconfig.php'
+  )
+  terminus remote:drush "${SITE}.${MULTIDEV}" php-script getconfig.php -- pantheon > "${PHP}"-config
+  sftp -o Port=2222 "${HOST}":code/ <<< $'rm getconfig.php'
+done
