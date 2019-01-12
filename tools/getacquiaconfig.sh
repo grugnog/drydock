d61fb163183d1aea9278ddc719fd38f8116825ac
Owen Barton <owen.barton@civicactions.com>
2019-01-11 17:59:29 -0800

    Acquia php 7.1 and 7.2 builds.

--- tools/getacquiaconfig.sh
+++ tools/getacquiaconfig.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail
+SUPPORTED_PHP=( 7.1 7.2 )
 if [ -z "$HOST" ]; then
   echo 'Specify a $HOST to get configuration for.'
   exit 1
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 scp "$DIR"/getconfig.php "${HOST}":/tmp/
-ssh "$HOST" php /tmp/getconfig.php acquia > currentconfig
+for PHP in "${SUPPORTED_PHP[@]}"; do
+  echo "Getting config for $HOST -> $PHP"
+  ssh "$HOST" "/usr/local/php${PHP}/bin/php" /tmp/getconfig.php acquia > "${PHP}"-config
+done
 ssh "$HOST" rm /tmp/getconfig.php
